ddd52b85a89970371db471571a82a4b891eab177
Preetam Varun <preetamarun99@gmail.com>
2021-11-20 12:44:06 +0530

    refactor code

--- main.js
+++ main.js
@@ -5,68 +5,42 @@ const passwordField = document.getElementById('password');
 const confirmPasswordField = document.getElementById('confirmPassword');
 const signUp = document.querySelector('form button');
 
+let errorMsg,currentField,passMsg;
+
+function showError(fieldName){
+    currentField = fieldName;
+    currentField.style.border = '2px solid red';
+    errorMsg = currentField.parentElement.lastElementChild;
+    currentField.name === 'FirstName' || currentField.name === 'LastName' ? 
+    errorMsg.textContent = `${currentField.name.toLowerCase()} should be atleast 5 characters` : 
+    currentField.name === 'email' ? errorMsg.textContent = 'not a valid email' : currentField.name === 'password' ?
+    errorMsg.textContent = 'password should be atleast 6 characters' : errorMsg.textContent = 'passwords do not match';
+    errorMsg.style.color = 'red';
+}
+
+function showPass(fieldName){
+    passMsg = fieldName.parentElement.lastElementChild;
+    fieldName.style.border = '2px solid green';
+    passMsg.textContent = '';    
+}
 
 function checkNames(){
-    if(fId.value.length >= 5){
-        let msg = fId.parentElement.lastElementChild;
-        fId.style.border = '2px solid green';
-        msg.textContent = '';
-    }
-    if(fId.value.length < 5){
-        fId.style.border = '2px solid red';
-        let msg = fId.parentElement.lastElementChild;
-        msg.textContent = 'First name should be atleast 5 characters';
-        msg.style.color = 'red';
-    }
-    if(lId.value.length >= 5){
-        lId.style.border = '2px solid green';
-        let msg = lId.parentElement.lastElementChild;
-        msg.textContent = '';
-    } 
-    if(lId.value.length < 5){
-        lId.style.border = '2px solid red';
-        let msg = lId.parentElement.lastElementChild;
-        msg.textContent = 'Last name should be atleast 5 characters';
-        msg.style.color = 'red';
-    }
+    fId.value.length >= 5 ? showPass(fId) : showError(fId);
+    lId.value.length >=5 ? showPass(lId) : showError(lId);
 }
 
 function checkEmail(){
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    if(re.test(String(emailInput.value).toLowerCase())){
-        emailInput.style.border = '2px solid green';
-    } else{
-        emailInput.style.border = '2px solid red';
-        emailInput.parentElement.lastElementChild.textContent = 'email is not valid';
-        emailInput.parentElement.lastElementChild.style.color = 'red';
-    }
+    (re.test(String(emailInput.value).toLowerCase())) ? showPass(emailInput) : showError(emailInput);
 }
 
 function checkConfirmPassword(password){
-    console.log('enter into function');
-    let msg = confirmPasswordField.parentElement.lastElementChild;
-    console.log(`original password ${password}`);
-    console.log(`confirmed password ${confirmPasswordField.value}`);
-    if(password === confirmPasswordField.value){
-        msg.textContent = '';
-        confirmPasswordField.style.border = '2px solid green';
-    } else{
-        msg.textContent = 'passwords do not match';
-        confirmPasswordField.style.border = '2px solid red';
-    }
+    password === confirmPasswordField.value ? showPass(confirmPasswordField) : showError(confirmPasswordField);
 }
-
 
 function checkPassword(){
     let password = passwordField.value;
-    let msg = passwordField.parentElement.lastElementChild;
-    if(password.length >= 6){
-        passwordField.style.border = '2px solid green';
-        msg.textContent = '';
-    } else{
-        msg.textContent = 'Password should be atleast 6 characters';
-        passwordField.style.border = '2px solid red';
-    }
+    password.length >= 6 ? showPass(passwordField) : showError(passwordField);
     checkConfirmPassword(password);
 }
 
@@ -76,6 +50,5 @@ function checkRequirements(e){
     checkPassword();
     e.preventDefault();
 }
-
 
 signUp.addEventListener('click',checkRequirements);
